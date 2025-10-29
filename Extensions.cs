7fc082fa2d4f5827f567012322541e1a0416110b
168iroha <168iroha@168iroha.net>
2025-10-29 20:37:29 +0900

    IEnumerable<TSource>.Prependを利用しなくなったため該当の拡張メソッドの削除

--- Extensions.cs
+++ Extensions.cs
@@ -17,16 +17,6 @@ static class Extensions
         result = default!;
         return false;
     }
-
-    public static IEnumerable<TSource> Prepend<TSource>(this IEnumerable<TSource> source, TSource element)
-    {
-        yield return element;
-
-        foreach (var item in source)
-        {
-            yield return item;
-        }
-    }
 }
 
 namespace System.Runtime.CompilerServices
